7ce43905e3bea4be2bb45cb2032675d2c34ae94f
Angel <mra7588@gmail.com>
2023-04-17 03:36:08 -0700

    add url image sps

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
         <div className="h-full flex flex-row flex-wrap justify-center items-center gap-2">
             <p className="font-medium text-lg text-gray-700">Created by Angel Muñoz</p>
             <Link to='https://spsolutions.com.mx/' target="_blank" className="w-40 h-16">
-                <img src="/src/assets/spsLogo.png" alt="SPSLogo" className='w-full h-full object-contain' />
+                <img src="https://res.cloudinary.com/dl9e7nlbu/image/upload/v1681727355/spsLogo_iuxk1o.png" alt="SPSLogo" className='w-full h-full object-contain' />
             </Link>
         </div>
     </footer>
